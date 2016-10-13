d319770ecd97f9e875487da98e41901a6ebbcf42
Sebastian Thiebaud <sebcthiebaud@gmail.com>
2016-10-12 21:43:01 -0700

    Use time to manage animations queue

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,7 +4,6 @@ import logging.config
 from structure  import *
 from animations import *
 from server     import Server
-from animstack  import *
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger("playasign")
--- lib/animations/main.py
+++ lib/animations/main.py
@@ -18,22 +18,28 @@ class Animation(object):
 class AnimationStack(object):
     def __init__(self):
         self.__current_animations = []
-        self.__delayed_animations = []
+        self.__queued_animations  = []
 
         self.animated = True
+
+        self._last_animation_end = None
+
         logger.info("new animation stack created: " + str(self))
 
     def add(self, animation, delay=0, duration=None, dismiss=False):
-        start = time.time() + delay
+        if self._last_animation_end == None:
+            start = time.time() + delay
+        else:  
+            start = self._last_animation_end + delay
 
         if duration == None:
             end = time.time() + 1000000000000000
         else:
             end = start + duration
+            self._last_animation_end = end
 
-        print str(start) + "  - -  " + str(end)
-        
         new_animation = Animation(animation=animation, start=start, end=end, dismiss=dismiss)
+        self.__queued_animations.append(new_animation)
 
         logger.info("new animation added to the stack: " + str(animation))
 
@@ -47,13 +53,16 @@ class AnimationStack(object):
             if animation.end <= time.time():
                 self.__current_animations.remove(animation)
 
+                if len(self.__current_animations) == 0:
+                    self._last_animation_end = None
+
                 if animation.dismiss == True:
                     Server().clean()
 
-        for animation in self.__delayed_animations:
+        for animation in self.__queued_animations:
             if animation.start <= time.time():
                 self.__current_animations.append(animation)
-                self.__delayed_animations.remove(animation)
+                self.__queued_animations.remove(animation)
 
     def __repr__(self):
-        return "animation stack (active: " + str(len(self.__current_animations)) + " - queue: + " + str(len(self.__delayed_animations)) + ")"
+        return "animation stack (active: " + str(len(self.__current_animations)) + " - queue: + " + str(len(self.__queued_animations)) + ")"
