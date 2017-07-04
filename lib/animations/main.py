c093dca2e63386b8025669f66ab15cd817ac2b6a
Sebastian Thiebaud <sebastian.thiebaud@postmates.com>
2017-07-04 13:15:18 -0700

    Update animation stack

--- lib/animations/main.py
+++ lib/animations/main.py
@@ -9,67 +9,29 @@ from ..server import Server
 logger = logging.getLogger("playasign.animations")
 
 class Animation(object):
-    def __init__(self, animation, start, end, dismiss):
+    def __init__(self, animation):
         self.animation = animation 
-        self.start     = start
-        self.end       = end 
-        self.dismiss   = dismiss
 
 class AnimationStack(object):
     def __init__(self):
-        self.__current_animations = []
-        self.__queued_animations  = []
-
-        self.hidden = False
-
-        self._last_animation_end = None
-
+        self.__animations = []
         logger.info("new animation stack created: " + str(self))
 
     def clear(self):
-        self.__current_animations = []
-        self.__queued_animations  = []
+        self.__animations = []
         
         Server().clean()
         logger.info("animation stack cleared")
 
-    def add(self, animation, delay=0, duration=None, dismiss=False):
-        if self._last_animation_end == None:
-            start = time.time() + delay
-        else:  
-            start = self._last_animation_end + delay
-
-        if duration == None:
-            end = time.time() + 1000000000000000
-        else:
-            end = start + duration
-            self._last_animation_end = end
-
-        new_animation = Animation(animation=animation, start=start, end=end, dismiss=dismiss)
-        self.__queued_animations.append(new_animation)
+    def add(self, animation):
+        new_animation = Animation(animation=animation)
+        self.__animations.append(new_animation)
 
         logger.info("new animation added to the stack: " + str(animation))
 
     def render(self):
-        if self.hidden == True:
-            return 
-
-        for animation in self.__current_animations:
+        for animation in self.__animations:
             animation.animation.render()
 
-            if animation.end <= time.time():
-                self.__current_animations.remove(animation)
-
-                if len(self.__current_animations) == 0:
-                    self._last_animation_end = None
-
-                if animation.dismiss == True:
-                    Server().clean()
-
-        for animation in self.__queued_animations:
-            if animation.start <= time.time():
-                self.__current_animations.append(animation)
-                self.__queued_animations.remove(animation)
-
     def __repr__(self):
-        return "animation stack (active: " + str(len(self.__current_animations)) + " - queue: + " + str(len(self.__queued_animations)) + ")"
+        return "animation stack (" + str(len(self.__animations)) + " active)"
